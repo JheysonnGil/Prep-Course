58bb4fb60a6c324fd85edc3d09d29e280a635b3b
Jheysonn Gil <jheysonnm@gmail.com>
2022-03-14 10:21:19 -0500

    update file

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -10,7 +10,7 @@ const nuevoNum = 1234;
 const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - 5 === 5;
+const nuevaResta = 11 - 5 === 6;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * 4 === 40;
